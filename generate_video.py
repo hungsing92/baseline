13b1ddefa23e33659e5c8abbe56ff7fa3d01a528
hungsing92 <h15521155576@gmail.com>
2017-07-03 15:52:43 +0800

    _

--- generate_video.py
+++ generate_video.py
@@ -15,9 +15,9 @@ num_frames=len(files_list)
 video_path=data_root+'seg/result_video.avi'
 fps = 24
 fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-videoWriter = cv2.VideoWriter(video_path,fourcc,fps,(1200,1000))#最后一个是保存图片的尺寸
+videoWriter = cv2.VideoWriter(video_path,fourcc,fps,(1600,1200))#最后一个是保存图片的尺寸
 
-fig, axes = plt.subplots(2, 1, figsize=(12, 10))
+fig, axes = plt.subplots(2, 1, figsize=(16, 12))
 ax0, ax1 = axes.ravel()
 
 
@@ -27,19 +27,21 @@ ax0, ax1 = axes.ravel()
 # fig2 = plt.subplot(2,1,2)
 # fig=plt.figure()
 for i in range(num_frames):
-	# fig1 = plt.subplot(2,1,1)
-	# fig2 = plt.subplot(2,1,2)
-	mFig = mpimg.imread(data_root+'seg/mayavi_fig/mayavi_%05d.png'%i)
-	rgbFig=mpimg.imread(data_root+'seg/result_rgb/rgb_%05d.png'%i)
-	ax0.imshow(mFig)
-	ax0.axis('off')
-	ax1.imshow(rgbFig)
-	ax1.axis('off')
-	# fig1.imshow(mFig)
-	# fig2.imshow(rgbFig)
-	plt.tight_layout()
-	plt.axis('off')
-	plt.savefig(data_root+'seg/result_video_img/result_video_img_%05d.png'%i)
+	# i=i+300
+	# fig, axes = plt.subplots(2, 1, figsize=(16, 12))
+	# ax0, ax1 = axes.ravel()
+	# mFig = mpimg.imread(data_root+'seg/mayavi_fig/mayavi_%05d.png'%i)
+	# rgbFig=mpimg.imread(data_root+'seg/result_rgb/rgb_%05d.png'%i)
+	# ax0.imshow(mFig)
+	# ax0.axis('off')
+	# ax1.imshow(rgbFig)
+	# ax1.axis('off')
+	# # fig1.imshow(mFig)
+	# # fig2.imshow(rgbFig)
+	# plt.tight_layout()
+	# plt.axis('off')
+	# plt.savefig(data_root+'seg/result_video_img/result_video_img_%05d.png'%i)
+	# plt.close()
 	# fig.show()
 	img=cv2.imread(data_root+'seg/result_video_img/result_video_img_%05d.png'%i)
 	videoWriter.write(img)
