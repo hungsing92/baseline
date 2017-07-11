7ad6ad7996b2f0c72a92b4c2ca15d54db81efaee
hungsing92 <h15521155576@gmail.com>
2017-07-11 16:52:03 +0800

    Some changes

--- tools/train_ResNet_vgg_double_up_s.py
+++ tools/train_ResNet_vgg_double_up_s.py
@@ -153,7 +153,8 @@ def run_train():
     out_dir = './outputs'
     makedirs(out_dir +'/tf')
     makedirs(out_dir +'/check_points')
-    log = Logger(out_dir+'/log_%s.txt'%(time.strftime('%Y-%m-%d %H:%M:%S')),mode='a')
+    makedirs(out_dir +'/log')
+    log = Logger(out_dir+'/log/log_%s.txt'%(time.strftime('%Y-%m-%d %H:%M:%S')),mode='a')
     index=np.load(data_root+'seg/train_list.npy')
     index=sorted(index)
     index=np.array(index)
