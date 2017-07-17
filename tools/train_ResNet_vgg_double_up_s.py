847f1aa35d6abf23f346c0f28ad11faec64fcfa1
hungsing92 <h15521155576@gmail.com>
2017-07-17 16:16:31 +0800

    Some changes

--- tools/train_ResNet_vgg_double_up_s.py
+++ tools/train_ResNet_vgg_double_up_s.py
@@ -221,28 +221,28 @@ def run_train():
         # sess = tf_debug.LocalCLIDebugWrapperSession(sess)
         # summary_writer = tf.summary.FileWriter(out_dir+'/tf', sess.graph)
         saver  = tf.train.Saver() 
-        # saver.restore(sess, './outputs/check_points/snap_R2R_contxt_065000.ckpt') 
+        saver.restore(sess, './outputs/check_points/snap_context__010000.ckpt') 
 
-        var_lt_res=[v for v in tf.trainable_variables() if v.name.startswith('res')]#resnet_v1_50
-        saver_0=tf.train.Saver(var_lt_res)        
-        saver_0.restore(sess, './outputs/check_points/resnet_v1_50.ckpt')
-        # # pdb.set_trace()
-        # top_lt=[v for v in tf.trainable_variables() if v.name.startswith('top_base')]
-        # top_lt.pop(0)
-        # # # top_lt.pop(0)
-        # for v in top_lt:
-        #     # pdb.set_trace()
-        #     for v_rgb in var_lt_res:
-        #         if v.name[9:]==v_rgb.name:
-        #             print ("assign weights:%s"%v.name)
-        #             v.assign(v_rgb)
+        # var_lt_res=[v for v in tf.trainable_variables() if v.name.startswith('res')]#resnet_v1_50
+        # saver_0=tf.train.Saver(var_lt_res)        
+        # saver_0.restore(sess, './outputs/check_points/resnet_v1_50.ckpt')
+        # # # pdb.set_trace()
+        # # top_lt=[v for v in tf.trainable_variables() if v.name.startswith('top_base')]
+        # # top_lt.pop(0)
+        # # # # top_lt.pop(0)
+        # # for v in top_lt:
+        # #     # pdb.set_trace()
+        # #     for v_rgb in var_lt_res:
+        # #         if v.name[9:]==v_rgb.name:
+        # #             print ("assign weights:%s"%v.name)
+        # #             v.assign(v_rgb)
 
-        var_lt_vgg=[v for v in tf.trainable_variables() if v.name.startswith('vgg')]
-        var_lt_vgg.pop(0)
-        saver_1=tf.train.Saver(var_lt_vgg)
+        # var_lt_vgg=[v for v in tf.trainable_variables() if v.name.startswith('vgg')]
+        # var_lt_vgg.pop(0)
+        # saver_1=tf.train.Saver(var_lt_vgg)
         
-        # # pdb.set_trace()
-        saver_1.restore(sess, './outputs/check_points/vgg_16.ckpt')
+        # # # pdb.set_trace()
+        # saver_1.restore(sess, './outputs/check_points/vgg_16.ckpt')
 
         batch_top_cls_loss =0
         batch_top_reg_loss =0
