e19a4b7a190b634fa5fb41057951b8fb3cfe46f2
hungsing92 <h15521155576@gmail.com>
2017-07-23 18:30:31 +0800

    Some changes

--- tools/train_ResNet_vgg_double_up_s.py
+++ tools/train_ResNet_vgg_double_up_s.py
@@ -241,7 +241,7 @@ def run_train():
         # sess = tf_debug.LocalCLIDebugWrapperSession(sess)
         # summary_writer = tf.summary.FileWriter(out_dir+'/tf', sess.graph)
         saver  = tf.train.Saver() 
-        saver.restore(sess, './outputs/check_points/snap_R2R_015000.ckpt') 
+        saver.restore(sess, './outputs/check_points/snap_R2R_040000.ckpt') 
 
         # var_lt_res=[v for v in tf.trainable_variables() if v.name.startswith('resnet_v1')]#resnet_v1_50
         # saver_0=tf.train.Saver(var_lt_res)        
@@ -277,7 +277,7 @@ def run_train():
         batch_top_reg_loss =0
         batch_fuse_cls_loss=0
         batch_fuse_reg_loss=0
-        rate=0.0002
+        rate=0.00005
         frame_range = np.arange(num_frames)
         idx=0
         frame=0
